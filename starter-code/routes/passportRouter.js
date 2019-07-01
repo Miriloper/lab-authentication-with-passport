182b9a128a41e31739b36f2c7ff9c6bcb753908c
Miriloper <miriam.mad.ml@gmail.com>
2019-07-01 18:49:44 +0200

    finish

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,17 +1,93 @@
-const express        = require("express");
+const express = require("express");
 const passportRouter = express.Router();
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const ensureLogin = require("connect-ensure-login");
+
 // Require user model
+const User = require("../models/user");
 
 // Add bcrypt to encrypt passwords
 
-// Add passport 
+// Add passport
 
+// const ensureLogin = require("connect-ensure-login");
 
-const ensureLogin = require("connect-ensure-login");
+// pass
 
-
-passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
-  res.render("passport/private", { user: req.user });
+passportRouter.get("/signup", (req, res, next) => {
+  res.render("passport/signup.hbs");
 });
+
+passportRouter.post("/new-user", (req, res, next) => {
+	const username = req.body.username;
+	const password = req.body.password;
+
+	if (username === "" || password === "") {
+		res.render("/signup", {
+			"message": "Indicate username and password",
+			"section": "signup"
+		});
+		return;
+	}
+
+	User.findOne({
+		username
+	})
+		.then(user => {
+			if (user !== null) {
+				res.render("/signup", {
+					"message": "The username already exists",
+					"section": "signup"
+				});
+				return;
+			}
+
+			const salt = bcrypt.genSaltSync(bcryptSalt);
+			const hashPass = bcrypt.hashSync(password, salt);
+
+			const newUser = new User({
+				username,
+				password: hashPass
+			});
+
+			newUser.save((err) => {
+				if (err) {
+					res.render("/signup", {
+						message: "Something went wrong",
+						"section": "signup"
+					});
+				} else {
+					res.redirect("/login");
+				}
+			});
+		})
+		.catch(error => {
+			next(error)
+		})
+});
+
+passportRouter.get(
+  "/private-page",
+  ensureLogin.ensureLoggedIn(),
+  (req, res) => {
+    res.render("passport/private", { user: req.user });
+  }
+);
+
+passportRouter.get("/login", (req, res, next) => {
+  res.render("passport/login.hbs");
+});
+
+
+
+passportRouter.post("/login-user", passport.authenticate("local", {
+    successReturnToOrRedirect: "/private-page",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true
+  }));
 
 module.exports = passportRouter;
